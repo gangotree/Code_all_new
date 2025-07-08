5ea89c2b25c05f57226bb7c97675188acf1c2d2f
gangotree <51043316+gangotree@users.noreply.github.com>
2025-07-08 09:52:02 +0530

    Add files via upload

--- prob_colvar_distribution.py
+++ prob_colvar_distribution.py
@@ -6,21 +6,21 @@ import seaborn as sns
 data_list = []
 def frange(start, stop, step):
     return [round(start + i * step, 1) for i in range(int((stop - start) / step) + 1)]
-dist_values = frange(1.2, 1.5, 0.1)
+dist_values = frange(1.0, 3.0, 0.1)
 for val in dist_values:
-    filename = f"COLVAR-K1000/COLVAR_{val}"
+    filename = f"COLVAR-K5000/COLVAR_{val}"
     df = pd.read_csv(filename, delim_whitespace=True, comment='#', header=None)
     df.columns = ["time", "dist", "rest"]
     data_list.append(df)
 
 plt.figure(figsize=(5, 4))
+#color = ["blue", "red", "green", "yellow", "cyan", "orange"]
+colors = sns.color_palette("hsv", n_colors=21)
+
 for i, df in enumerate(data_list):
-    color = "red" if i < 4 else "blue"
-    sns.histplot(df["dist"], kde=True, stat="probability", color=color, bins=100)
+    sns.histplot(df["dist"], kde=True, stat="probability", color=colors[i], bins=100,alpha=0.5)
 
 plt.title('Probability')
 plt.xlabel('Rg')
 plt.ylabel('P(Rg)')
-plt.savefig('distribution-K1000.pdf', dpi=300)
-plt.show()
-
+plt.savefig(f"test.pdf", dpi=300)
